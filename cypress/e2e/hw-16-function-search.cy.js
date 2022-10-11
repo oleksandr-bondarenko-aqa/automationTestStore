1a096c6872bfd7475fbe1b791c368b7013468dae
Oleksandr Bondarenko <oleksandr.bondarenko.qa@gmail.com>
2022-10-11 21:24:48 +0300

    solved lesson 16 homework

---
+++ cypress/e2e/hw-16-function-search.cy.js
@@ -0,0 +1,24 @@
+import {getProductByName, productName} from "../support/helper";
+import user from "../fixtures/user.json";
+
+it (`Place order with product name: ${productName}`, () => {
+    cy.setCookie("AC_SF_8CEFDA09D5", user.AC_SF_8CEFDA09D5);
+
+    cy.visit('/');
+
+    cy.get('#filter_keyword')
+        .type('i')
+        .closest('form')
+        .submit();
+
+    getProductByName('Total Moisture Facial Cream');
+
+    cy.get('#product_quantity').clear().type('3');
+    cy.get('.productpagecart').click();
+    cy.get('#cart_checkout1').click();
+    cy.get('#checkout_btn').click();
+    cy.get('h1.heading1').should('contain', ' Your Order Has Been Processed!');
+    cy.get('.contentpanel')
+        .should('contain', 'Your order')
+        .and('contain', 'Thank you for shopping with us!')
+})
