83e9609371441307135c93be49a6a441f684a537
Oleksandr Bondarenko <oleksandr.bondarenko.qa@gmail.com>
2022-10-18 21:36:37 +0300

    added lesson 19 theory

--- cypress/e2e/homework/hw-16-function-search.cy.js
+++ cypress/e2e/homework/hw-16-function-search.cy.js
@@ -1,8 +1,11 @@
-import {getProductByName, productName} from "../../support/helper";
+import {getProductByName, getProductByNameWithFor, productName} from "../../support/helper";
 import user from "../../fixtures/user.json";
+import authorizationPage from "../../support/pages/AuthorizationPage";
 
-it (`Place order with product name: ${productName}`, () => {
-    cy.setCookie("AC_SF_8CEFDA09D5", user.AC_SF_8CEFDA09D5);
+it (`Place order with product name`, () => {
+    authorizationPage.visit();
+
+    authorizationPage.submitLoginForm(user.userName, user.password);
 
     cy.visit('/');
 
@@ -12,6 +15,30 @@ it (`Place order with product name: ${productName}`, () => {
         .submit();
 
     getProductByName('Total Moisture Facial Cream');
+
+    cy.get('#product_quantity').clear().type('3');
+    cy.get('.productpagecart').click();
+    cy.get('#cart_checkout1').click();
+    cy.get('#checkout_btn').click();
+    cy.get('h1.heading1').should('contain', ' Your Order Has Been Processed!');
+    cy.get('.contentpanel')
+        .should('contain', 'Your order')
+        .and('contain', 'Thank you for shopping with us!')
+})
+
+it (`Place order with product name with for`, () => {
+    authorizationPage.visit();
+
+    authorizationPage.submitLoginForm(user.userName, user.password);
+
+    cy.visit('/');
+
+    cy.get('#filter_keyword')
+        .type('i')
+        .closest('form')
+        .submit();
+
+    getProductByNameWithFor('Total Moisture Facial Cream');
 
     cy.get('#product_quantity').clear().type('3');
     cy.get('.productpagecart').click();
--- cypress/support/helper.js
+++ cypress/support/helper.js
@@ -9,13 +9,32 @@ export function searchExistingProduct(productName) {
 }
 
 export function getProductByName(productName) {
-
     cy.get('body').then((body) => {
         if (body.find(`[title="${productName}"]`).length) {
             cy.get(`[title="${productName}"]`).click();
         } else {
             cy.get('.pagination a').contains('>').click();
             getProductByName(productName);
+        }
+    })
+}
+
+export function getProductByNameWithFor (productName) {
+    cy.get('.pagination a').then(pages => {
+        return pages.length;
+    }).then(pageCount => {
+        for (let i = 0; i < pageCount; i++){
+            cy.location().then(location => {
+                if (!location.search.includes('product/product')){
+                    cy.get('body').then((body) => {
+                        if (body.find(`[title="${productName}"]`).length) {
+                            cy.get(`[title="${productName}"]`).click();
+                        } else {
+                            cy.get('.pagination a').contains('>').click();
+                        }
+                    })
+                }
+            })
         }
     })
 }
