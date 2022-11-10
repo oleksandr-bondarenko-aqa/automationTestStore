74ede5ddaf2478db4d50114113ecd9a860c92855
Oleksandr Bondarenko <oleksandr.bondarenko.qa@gmail.com>
2022-11-10 21:14:31 +0200

    added lesson 23 theory

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require("cypress");
 
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
 module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
@@ -8,6 +10,8 @@ module.exports = defineConfig({
     baseUrl: 'https://automationteststore.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      allureWriter(on, config);
+      return config;
     },
   },
 });
